67382e716afa2ae8ab5634c8bc3e02e27b3c2cb0
Jaden2006 <131674929+Jaden2006@users.noreply.github.com>
2023-05-22 13:43:49 +0200

    Add files via upload

---
+++ php1.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ 
+</head>
+
+<body>
+
+
+<?php
+  $getal1 = 12;
+  $getal2 = 10;
+  $resultaat = $getal1 + $getal2;
+  echo "De som van " . $getal1 . " en " . $getal2 . " is " . $resultaat;
+?>
+
+<?php
+    $var1 = 12;
+    $var2 = 10;
+    $resultaat = $var1 * $var2;
+    echo $resultaat;
+?>
+
+<?php
+    $naam = "Jaden Emanuels";
+    echo "Welkom $naam!";
+?>
+
+
+
+
+
+
+
+
+
+</body>
---
+++ php2.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+ 
+</head>
+
+<body>
+
+<?php
+1.
+$variabele1 = 10;
+$variabele2 = 10;
+
+if ($variabele1 == $variabele2) {
+    echo "De twee waarden zijn gelijk";
+} else {
+    echo "De twee waarden zijn ongelijk";
+}
+
+2.
+$variabele1 = 10;
+$variabele2 = 12;
+
+if ($variabele1 != $variabele2) {
+    echo "De twee waarden zijn ongelijk";
+}
+
+
+3. 
+$variabele1 = 10;
+$variabele2 = 10;
+
+if ($variabele1 == $variabele2) {
+    echo "De twee waarden zijn gelijk";
+} else {
+    echo "De twee waarden zijn ongelijk";
+}
+
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
